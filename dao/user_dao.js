b6270a460fe09ba3b4f36a075fc627dcc09f745c
anujsharma <sharma.anuj15.github@gmail.com>
2022-04-15 00:35:14 -0400

    Session Management in process

--- dao/user_dao.js
+++ dao/user_dao.js
@@ -5,19 +5,28 @@ import { login } from "../service/user_service";
 export const createUser = (user) => {
     userModel.create(user);
 }
+
+
 export const login = () => {}
+
+
 export const deleteUser = (uid) => {
     userModel.deleteOne(uid)
 }
+
 export const updateUser = (user) => {}
 
 export const createUserDetails = (user, userDetails) => {
     userDetailsModel.create(userDetails);
 }
+
+
 export const getUserDetails = (uid) => {
     userDetailsModel.find(uid);
 }
+
 export const updateUserDetails = (user) => {}
+
 export const deleteUserDetails = (uid) => {
     userDetailsModel.deleteOne(uid);
 }
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import session from 'express-session';
+import MongoDBSession from 'connect-mongodb-session';
+import mongoose from 'mongoose';
+
+const mongoDBSession = MongoDBSession(session);
+
+const mongoURI = 'mongodb+srv://food:food@cluster0.vprdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI)
+.then((res) => {
+    console.log("MongoDB connected")
+})
+
+const store = new mongoDBSession({
+    uri: mongoURI,
+    collection: "mySessions",
+})
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(session({
+    secret: 'key that will sign cookie',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+}))
+
 app.get('/', (req, res) => {
+    req.session.isAuth = true;
     res.send("Server is running OK.")
 });
 
