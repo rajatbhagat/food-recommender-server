c090ac9a84fd87615c2a35c98661d648301fc998
anujsharma <sharma.anuj15.github@gmail.com>
2022-04-19 23:33:10 -0400

    Edit Profile completed

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,23 +1,26 @@
 import userModel from '../models/user_model.js';
+import { updateDaoUser } from '../dao/user_dao.js';
 import bcrypt from 'bcryptjs';
-
+import CryptoJS from 'crypto-js';
+const SECRET_KEY = 'FOOD';
 const userController = (app) => {
     app.get('/api/users', findAllUsers);
     app.get('/api/users/:uid', findUserById);
     app.post('/api/users/createUser', createUser);
-    // app.delete('/api/users/:uid', deleteUser);
-    // app.put('/api/users/:uid', updateUser);
+    app.post('/api/users/loginUser', loginUser);
+    app.put('/api/users/:uid', updateUser);
 }
 
-// const updateUser = (req, res) => {
-//     const userId = req.params['uid'];
-//     const updatedUser = req.body;
-//     users = users.map(usr =>
-//       usr._id === userId ?
-//       updatedUser :
-//       usr);
-//     res.send(200);
-//    }
+const updateUser = async (req, res) => {
+    console.log("Starting update user")
+    const userId = req.params['uid'];
+    const updatedUser = req.body;
+    console.log(updatedUser)
+    console.log(userId)
+    const out = updateDaoUser(userId,updatedUser);
+    console.log(out);
+    res.send(200);
+  }
    
 
 // const deleteUser = (req, res) => {
@@ -26,23 +29,30 @@ const userController = (app) => {
 //       usr._id !== userId);
 //     res.send(200);
 //    }
-   
 
 const createUser = async (req, res) => {
     const newUser = req.body;
     console.log("Server: ",req.body);
-    // let user = await userModel.findOne(newUser.name)
-    // console.log("User",user)
-    // if(user) {
-    //     return res.send(400);
-    // }
-
-    const hashedPwd = await bcrypt.hash(newUser.password, 12);
-    newUser.password = hashedPwd; 
     const responseUser = userModel.create(newUser);
 
     res.send(responseUser);
    }
+
+const loginUser = async (req, res) => {
+  const loggedInUser = req.body;
+  // const hashedPwd = await CryptoJS.AES.encrypt(loggedInUser.password, SECRET_KEY);
+  // console.log("Pwd", hashedPwd)
+  const dbDetails = await userModel.find({email: loggedInUser.email});
+  // let bytes  = CryptoJS.AES.decrypt(dbDetails.password, SECRET_KEY);
+  // let decryptedPwd = bytes.toString(CryptoJS.enc.Utf8);
+  console.log(dbDetails[0].password)
+  console.log(loggedInUser.password)
+  if (dbDetails && dbDetails[0].password === loggedInUser.password) {
+    return res.send(dbDetails[0]);
+  } else {
+    return res.send("User Not Found");
+  }
+}
    
 
 const findUserById = (req, res) => {
